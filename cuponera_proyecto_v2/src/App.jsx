d0401de971f942e6d84b4881ca5de0580efe7772
andcobos <146792784+andcobos@users.noreply.github.com>
2025-02-28 17:32:19 -0600

    creacion de proyecto y tailwind

---
+++ cuponera_proyecto_v2/src/App.jsx
@@ -0,0 +1,11 @@
+export const App = () => {
+    return (
+        <>
+        <div className="bg-sky-200 h-screen flex items-center justify-center">
+            <h1 className="text-black text-4xl">Tailwind</h1>
+        </div>
+        </>
+    )
+}
+
+export default App
